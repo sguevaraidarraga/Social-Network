9db2ee0003eaf907ced5a36b702fa7d788052dfe
Santiago Guevara <sguevaraidarraga@gmail.com>
2025-03-17 18:42:15 -0500

    changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import { FaHome, FaUser, FaBell, FaCog } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaHome, FaBell, FaCog, FaBars } from "react-icons/fa";
 import "../styles/Sidebar.css";
 
 function Sidebar() {
+  const [showLogout, setShowLogout] = useState(false);
+
   return (
     <div className="sidebar">
+      <div className="sidebar-top">
         <h2>Social Network</h2>
         <ul>
-            <li><FaHome /> Home</li>
-            <li><FaUser /> Profile</li>
-            <li><FaBell /> Notifications</li>
-            <li><FaCog /> Settings</li>
+          <li><FaHome /> Home</li>
+          <li><FaBell /> Notifications</li>
+          <li><FaCog /> Settings</li>
         </ul>
+      </div>
+      
+      {/* Profile Section - Fixed at the Bottom */}
+      <div className="sidebar-profile">
+        <li className="profile-item">
+          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="User" className="profile-image" /> Profile
+          <FaBars className="options-icon" onClick={() => setShowLogout(!showLogout)} />
+        </li>
+        {showLogout && (
+          <div className="logout-popup">
+            <button className="logout-button">Log Out</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
